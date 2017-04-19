a41ec6ddbb5f2cd3138d0b998c62812919e84017
Maksim Zakharenka <Maksim_Zakharenka@epam.com>
2017-04-19 20:15:07 +0300

    Add tab layout and design app and add database UI item

--- qrscanner/app/src/main/java/com/github/maxcriser/qrscanner/MainActivity.java
+++ qrscanner/app/src/main/java/com/github/maxcriser/qrscanner/MainActivity.java
@@ -1,19 +1,99 @@
 package com.github.maxcriser.qrscanner;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.maxcriser.qrscanner.adapter.SampleFragmentPagerAdapter;
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private static final int PERMISSION_REQUEST_CAMERA = 0;
+    private ZXingScannerView mZXingScannerView;
+    private Bundle mBunlde;
+
+    @Override
+    public final void handleResult(final Result pResult) {
+        Toast.makeText(this, pResult.getText(), Toast.LENGTH_SHORT).show();
+    }
 
     public void onScanClicked(View view) {
-        this.startActivity(new Intent(this, ScannerActivity.class));
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    PERMISSION_REQUEST_CAMERA);
+        } else {
+            startScanner();
+        }
+    }
+
+    private void startScanner() {
+        startActivity(new Intent(this, ScannerActivity.class));
+//        mZXingScannerView = new ZXingScannerView(this);
+//        setContentView(mZXingScannerView);
+//        mZXingScannerView.setResultHandler(this);
+//        mZXingScannerView.startCamera();
+//        mZXingScannerView.toggleFlash();
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    private void initViews() {
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(), this));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBunlde = savedInstanceState;
+        initViews();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Application need access to the camera", Toast.LENGTH_SHORT).show();
+        } else {
+            startScanner();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mZXingScannerView != null) {
+            mZXingScannerView.stopCamera();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mZXingScannerView != null) {
+            mZXingScannerView.startCamera();
+        }
     }
 }
--- qrscanner/app/src/main/java/com/github/maxcriser/qrscanner/ScannerActivity.java
+++ qrscanner/app/src/main/java/com/github/maxcriser/qrscanner/ScannerActivity.java
@@ -1,14 +1,9 @@
 package com.github.maxcriser.qrscanner;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.google.zxing.Result;
@@ -17,42 +12,20 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
-    private static final int PERMISSION_REQUEST_CAMERA = 0;
     private ZXingScannerView mZXingScannerView;
 
     @Override
-    public final void handleResult(final Result pResult) {
+    public void handleResult(final Result pResult) {
         Toast.makeText(this, pResult.getText(), Toast.LENGTH_SHORT).show();
-    }
-
-    public void onScanClicked(final View view) {
-        mZXingScannerView = new ZXingScannerView(this);
-        setContentView(mZXingScannerView);
-        mZXingScannerView.setResultHandler(this);
-        mZXingScannerView.startCamera();
-        mZXingScannerView.toggleFlash();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-            startActivity(new Intent(this, MainActivity.class));
-            Toast.makeText(this, "Application need access to the camera", Toast.LENGTH_SHORT).show();
-            finish();
-        }
     }
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscanner);
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA},
-                    PERMISSION_REQUEST_CAMERA);
-        }
+        mZXingScannerView = (ZXingScannerView) findViewById(R.id.zxingscanner_id);
+        mZXingScannerView.startCamera();
+        mZXingScannerView.setResultHandler(this);
     }
 
     @Override
