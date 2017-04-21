17d1cc1c0ba636436a9395bb1a4575cde732fb78
Maksim Zakharenka <Maksim_Zakharenka@epam.com>
2017-04-21 16:50:52 +0300

    Refresh

--- qrscanner/app/src/main/java/com/github/maxcriser/qrscanner/adapter/SampleFragmentPagerAdapter.java
+++ qrscanner/app/src/main/java/com/github/maxcriser/qrscanner/adapter/SampleFragmentPagerAdapter.java
@@ -1,9 +1,11 @@
 package com.github.maxcriser.qrscanner.adapter;
 
+import android.app.Application;
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.LoaderManager;
 
 import com.github.maxcriser.qrscanner.fragment.PageFragment;
 
@@ -11,9 +13,13 @@ public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
     final int PAGE_COUNT = 2;
     private Context context;
     private String tabTitles[] = new String[] { "Scanning", "DATABASE" };
+    private LoaderManager mLoaderManager;
+    private Application mApplication;
 
-    public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+    public SampleFragmentPagerAdapter(FragmentManager fm, Context context, Application pApplication, LoaderManager pLoaderManager) {
         super(fm);
+        mApplication = pApplication;
+        mLoaderManager = pLoaderManager;
         this.context = context;
     }
 
@@ -24,7 +30,7 @@ public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return PageFragment.newInstance(position + 1);
+        return PageFragment.newInstance(position + 1, mLoaderManager, mApplication, context);
     }
 
     @Override
